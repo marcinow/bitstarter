1178b4599a7e573d5eca46fd3f7875ffddf66451
marcinow <krzysztof.marcinowicz@gmail.com>
2013-07-12 22:09:53 +0000

    serve index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 3!');
+  var content = fs.readFileSync("./index.html");
+  response.send(content.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
